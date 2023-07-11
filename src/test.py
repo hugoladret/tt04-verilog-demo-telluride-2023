0f0618c0849b7a5c531818a57ec933c138dbbb7b
Hugo Ladret <hugo.ladret@gmail.com>
2023-07-11 16:11:07 -0500

    updating test indexing method

--- src/test.py
+++ src/test.py
@@ -2,10 +2,9 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-# Define some test positions for the pong ball and expected states for the paddle
-ball_positions = [0, 10, 20, 30, 40, 50, 60]  # y-coordinates for the pong ball
-expected_states = [0, 10, 20, 30, 40, 50, 60]  # Expected paddle states (assuming initial state is 0 and paddle follows the ball perfectly)
-expected_spikes = [0, 1, 1, 1, 1, 1, 1]  # Neuron should spike when ball's y-coordinate exceeds paddle's y-coordinate
+ball_positions = [0, 10, 20, 30, 40, 50, 60]  
+expected_states = [0, 10, 20, 30, 40, 50, 60]  
+expected_spikes = [0, 1, 1, 1, 1, 1, 1]  
 
 @cocotb.test()
 async def test_neuron(dut):
@@ -25,11 +24,12 @@ async def test_neuron(dut):
         dut.ui_in.value = ball_positions[i]
         await ClockCycles(dut.clk, 10)
 
-        # Check the neuron's state (paddle's y-coordinate)
-        assert int(dut.uo_out[1:6].value) == expected_states[i]
+        # Convert the dut.uo_out.value to an integer and apply bit masking
+        uo_out_value = int(dut.uo_out.value)
+        uo_out_bits_1_to_6 = (uo_out_value >> 1) & 0x3F
+        spike_bit = uo_out_value & 1  # Extract the spike bit
 
-        # Check if the neuron spikes when it should
-        assert int(dut.uo_out[0].value) == expected_spikes[i]
+        assert uo_out_bits_1_to_6 == expected_states[i]
+        assert spike_bit == expected_spikes[i]
 
-        # All bidirectionals are set to output
         assert dut.uio_oe.value == 0xFF
